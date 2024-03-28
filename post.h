27312142804acf8c6eb4651c7bbe8870c83881df
Arnav <dixitarnav2@gmail.com>
2024-03-28 02:13:20 -0400

    Update post.h

--- post.h
+++ post.h
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <vector>
 #include <string>
 
 #ifndef POST_H
@@ -9,7 +9,7 @@ public:
 	
     // PRE: ID, Post message, likes
     // POST: initializes private variables
-	Post(int messageId, int ownerId, std::string message, int likes);
+    Post(int messageId, int ownerId, std::string message, int likes, int laughs, int dislikes, std::vector<int> l_names, std::vector<int> la_names, std::vector<int> d_names);
 
     // PRE: N/A
     // POST: Initializes all private fields to default values
@@ -43,18 +43,57 @@ public:
     // POST: Returns true since all regular posts are public
     virtual bool getIsPublic() const;
 
+    void addLikes();
+
+    void addLaughs();
+
+    void addDislikes();
+
+    std::vector<int> getLikedUsers();
+
+    std::vector<int> getLaughs();
+
+    std::vector<int> getDislikes();
+
+    void setLikedUsers(int id);
+
+    void setLaughs(int id);
+
+    void setDislikes(int id);
+
+    void subtractLikes();
+
+    void subtractLaughs();
+
+    void subtractDislikes();
+
+    void remove_likes();
+
+    void remove_laugh();
+
+    void remove_dislike();
+
+    int getLaugh();
+
+    int getDislike();
+
 private:
     int messageId_;
     int ownerId_;
     std::string message_;
     int likes_;
+    int laugh_count;
+    int dislike_count;
+    std::vector<int> liked_users;
+    std::vector<int> laughs;
+    std::vector<int> dislikes;
 };
 
 class IncomingPost : public Post {
 public:
     // PRE: MessageId, User's ID, Post's message, number of likes, boolean to represent whether post is public, author of post
     // POST: Initializes private variables
-    IncomingPost(int messageId, int ownerId, std::string message, int likes, bool isPublic, std::string author);
+    IncomingPost(int messageId, int ownerId, std::string message, int likes, int laughs, int dislikes, std::vector<int> like_list, std::vector<int> laugh_list, std::vector<int> dislike_list, bool isPublic, std::string author);
     
     // PRE: N/A
     // POST: Initializes all private variables to default values
